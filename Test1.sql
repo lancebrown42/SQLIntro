c5bb37751e6270091a110c00650c530a579861bb
Lance <lance.brown42@gmail.com>
2020-03-01 12:15:07 -0500

    data inserted

--- Test1.sql
+++ Test1.sql
@@ -98,8 +98,36 @@ FOREIGN KEY ( intNurseID ) REFERENCES TNurses ( intNurseID )
 --	Add Sample Data - INSERTS - Step 12
 -- --------------------------------------------------------------------------------
 INSERT INTO TPatients (intPatientID, strFirstName, strLastName, dtmDOB, strSSN, strInsurance, strPhone, strEmail, strEmergencyContact) 
-VALUES				 (1, 'Sylvan', 'Cosyns', '1/3/1970', '524-94-0926', 'Cartwright, MacGyver and Champlin', '850-665-0133', 'scosyns0@china.com.cn', 'Greg Cosyns');
-					 (2, 'Sig', 'Combs', '4/3/1972', '488-59-4935', 'Will, Swaniawski and Haag', '387-738-5732', 'scombs1@google.com.br', 'Sean Combs');
-					 (3, 'Hinze', 'Gorghetto', '3/20/1959', '341-62-0664', 'Koelpin, Wehner and Hettinger', '249-958-4640', 'hgorghetto2@google.de', 'Ralph Gorghetto');
-					 (4, 'Inez', 'Santry', '8/27/1906', '123-46-5785', 'Dicki, Lowe and Fritsch', '930-128-0781', 'isantry3@mysql.com', 'Onez Santry');
-					 (5, 'Andie', 'Toleman', '2/21/1976', '541-11-1169', 'Champlin and Sons', '642-137-5355', 'atoleman4@boston.com', 'Maria Toleman');
+VALUES				  (1, 'Sylvan', 'Cosyns', '1/3/1970', '524-94-0926', 'Cartwright, MacGyver and Champlin', '850-665-0133', 'scosyns0@china.com.cn', 'Greg Cosyns')
+					 ,(2, 'Sig', 'Combs', '4/3/1972', '488-59-4935', 'Will, Swaniawski and Haag', '387-738-5732', 'scombs1@google.com.br', 'Sean Combs')
+					 ,(3, 'Hinze', 'Gorghetto', '3/20/1959', '341-62-0664', 'Koelpin, Wehner and Hettinger', '249-958-4640', 'hgorghetto2@google.de', 'Ralph Gorghetto')
+					 ,(4, 'Inez', 'Santry', '8/27/1906', '123-46-5785', 'Dicki, Lowe and Fritsch', '930-128-0781', 'isantry3@mysql.com', 'Onez Santry')
+					 ,(5, 'Andie', 'Toleman', '2/21/1976', '541-11-1169', 'Champlin and Sons', '642-137-5355', 'atoleman4@boston.com', 'Maria Toleman')
+INSERT INTO TDoctors (intDoctorID, strFirstName, strLastName, dtmBoards, strAddress, strPhone, strEmail) 
+VALUES 				 (1, 'Shelia', 'McSkin', '9/30/1996', '6 Rutledge Court', '797-954-8306', 'smcskin0@paginegialle.it')
+					,(2, 'Maximilianus', 'Poe', '7/21/1962', '2739 Amoth Point', '594-525-2064', 'mpoe1@sciencedaily.com')
+					,(3, 'Verine', 'Choppen', '1/9/1982', '911 Summer Ridge Trail', '475-177-3555', 'vchoppen2@go.com')
+					,(4, 'Wesley', 'Byrne', '8/16/1983', '23613 Muir Crossing', '941-847-5450', 'wbyrne3@chron.com')
+					,(5, 'Gael', 'Leavy', '12/23/1956', '17 Porter Circle', '410-876-1555', 'gleavy4@clickbank.net')
+INSERT INTO TNurses (intNurseID, strFirstName, strLastName, dtmBoards, strAddress, strPhone, strEmail)
+VALUES				 (1, 'Micheal', 'Hansard', '6/28/1953', '3266 Mandrake Park', '584-261-6117', 'mhansard0@ebay.com')
+					,(2, 'Delmore', 'Singyard', '8/17/1976', '94787 Nancy Court', '598-519-0118', 'dsingyard1@tmall.com')
+					,(3, 'Karissa', 'Cappineer', '12/29/1994', '2 Maywood Trail', '453-477-8427', 'kcappineer2@ftc.gov')
+					,(4, 'Florian', 'Philo', '11/25/1973', '34 Colorado Plaza', '964-601-3141', 'fphilo3@state.gov')
+					,(5, 'Madelene', 'Peaurt', '10/20/1979', '46 Stone Corner Point', '282-306-3606', 'mpeaurt4@prweb.com')
+INSERT INTO TVisits (intVisitID, intPatientID, intDoctorID, dtmTime, strReason, strDiagnosis) 
+VALUES 				 (1, 1, 2, '2/15/1955', '4545 Roth Junction', '701-920-9322')
+					,(2, 5, 4, '6/25/1979', '0 Longview Court', '208-414-9920')
+					,(3, 4, 1, '2/12/1995', '34 Kedzie Avenue', '311-463-5298')
+					,(4, 4, 2, '3/4/1963', '7 Rieder Trail', '763-896-0714')
+					,(5, 5, 1, '8/5/1987', '320 Kensington Parkway', '587-450-6718')
+INSERT INTO TVisitNurses (intVisitNurseID, intVisitID, intNurseID)
+VALUES				 (1, 3, 5)
+					,(2, 2, 4)
+					,(3, 2, 1)
+					,(4, 4, 2)
+					,(5, 1, 3)
+					,(6, 5, 2)
+-- ------------------------------------------------------------------
+-- Select joins - Step 13
+-- ------------------------------------------------------------------
